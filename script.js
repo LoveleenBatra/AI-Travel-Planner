6d589ae3b3c15506dd370a65ad7dd1156a0731e2
Loveleen Batra <loveleenbatra0to9@gmail.com>
2025-10-15 03:52:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.getElementById("travelForm").addEventListener("submit", async function 
   }, 500); // change emoji every 0.5s
 
   try {
-    const response = await fetch("http://127.0.0.1:5000/plan", {
+    const response = await fetch(" https://ai-travel-planner-1-qdnd.onrender.com", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ destination, budget, days, pdf: wantPDF }),
@@ -62,3 +62,4 @@ document.getElementById("travelForm").addEventListener("submit", async function 
     console.error(error);
   }
 });
+
